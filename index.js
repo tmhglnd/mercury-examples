1dcb5a0f1accacdb457777afd3e72b7118741d2f
tmhglnd <timohoogland@gmail.com>
2023-10-12 23:13:22 +0200

    initial commit of examples and tutorials

---
+++ index.js
@@ -0,0 +1,9 @@
+//
+// index.js
+// main entry for tutorials and examples of Mercury language
+// by Timo Hoogland (@tmhglnd), www.timohoogland.com
+// 
+
+// require the json files and export to examples and tutorials
+exports.Examples = require('./data/examples.json');
+exports.Tutorials = require('./data/tutorials.json');

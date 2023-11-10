cfb60ac5454feab9d3609a26a5249de7086fac88
tmhglnd <timohoogland@gmail.com>
2023-11-10 10:33:44 +0100

    json inline in package

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 // by Timo Hoogland (@tmhglnd), www.timohoogland.com
 // 
 
-// require the json files and export to examples and tutorials
-exports.Examples = require('./data/examples.json');
-exports.Tutorials = require('./data/tutorials.json');
+const fs = require('fs');
+
+// read the json files and export to examples and tutorials
+// converted to inline json in the distribution through brfs
+exports.Examples = JSON.parse(fs.readFileSync('./data/examples.json', 'utf-8'));
+exports.Tutorials = JSON.parse(fs.readFileSync('./data/tutorials.json', 'utf-8'));
